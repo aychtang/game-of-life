9511085b2b372ce76695f19fcae778a6b34b53db
Howard Tang <tangh@google.com>
2014-02-22 21:42:05 +0000

    Working findneighbours implementation and test.

--- test.js
+++ test.js
@@ -2,7 +2,9 @@ var test = require('tape');
 var board = require('./board');
 var print = require('./boardPrinter');
 var Simulation = require('./simulation');
+var findNeighbours = require('./findNeighbours');
 
+// Board maker.
 test('Board maker should return an array with space for n^2', function(t) {
 	t.plan(2);
 	t.equal(board(3,0).length, 3);
@@ -15,12 +17,14 @@ test('Board maker should return an array where each element equals the passed ce
 		t.equal(board(1, 0)[0][0], 0);
 });
 
+// Printer.
 test('Board printer should return correct representation of passed board if passed correct row length',
 	function(t) {
 	t.plan(1);
 	t.equal(print(board(3,0)), '000\n000\n000\n');
 });
 
+// Simulation wrapper.
 test('Simulation should start on step 0', function(t) {
 	t.plan(1);
 	t.equal(new Simulation(board(3, 0)).step, 0);
@@ -34,4 +38,17 @@ test('Simulation.isDead() should return true if all cells equal 0', function(t) 
 test('Simulation.isDead() should return false if not all cells equal 0', function(t) {
 	t.plan(1);
 	t.equal(new Simulation(board(3, 1)).isDead(), false);
+});
+
+// findNeighbours.
+test('findNeighbours should return amount of living neighbour cells when passed valid input', function(t) {
+	t.plan(1);
+	var myBoard = board(3, 0);
+	t.equal(findNeighbours(myBoard, 0, 0), 0);
+});
+
+test('findNeighbours should return amount of living neighbour cells when passed valid input', function(t) {
+	t.plan(1);
+	var myBoard = board(3, 1);
+	t.equal(findNeighbours(myBoard, 0, 0), 3);
 });
