20ff732f78c48cf178e0418f57173d0c1dc33b8b
Howard Tang <tangh@google.com>
2014-02-22 23:20:02 +0000

    _.clone is not my friend.

--- step.js
+++ step.js
@@ -1,11 +1,11 @@
 var Simulation = require('./simulation');
 var findNeighbours = require('./findNeighbours');
+var makeBoard = require('./board');
 var _ = require('underscore');
-
 
 module.exports = function(simulation) {
 	var board = simulation.board;
-	var result = _.clone(board);
+	var result = makeBoard(simulation.board.length, 0);
 
 	for (var i = 0; i < board.length; i++) {
 		for (var j = 0; j < board.length; j++) {
@@ -19,9 +19,12 @@ module.exports = function(simulation) {
 				if (neighbours > 3 || neighbours < 2) {
 					result[i][j] = 0;
 				}
+				else {
+					result[i][j] = 1;
+				}
 			}
 		}
 	}
-	console.log(simulation.step);
+
 	return new Simulation(result, simulation.step + 1);
 };
